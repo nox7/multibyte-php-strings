027493c0737580195dc35e4a1439bd08bcbc5c4f
Garet C. Green <17110935+nox7@users.noreply.github.com>
2022-10-03 15:50:22 -0500

    Add unit test for multiple occurrences

--- tests/StubTest.php
+++ tests/StubTest.php
@@ -2,6 +2,7 @@
 	require_once __DIR__ . "/../vendor/autoload.php";
 
 	use MultiByteString\MultiByteString;
+	use MultiByteString\StubResult;
 	use PHPUnit\Framework\TestCase;
 
 	class StubTest extends TestCase
@@ -24,7 +25,7 @@
 			);
 
 			$this->assertInstanceOf(
-				expected: \MultiByteString\StubResult::class,
+				expected: StubResult::class,
 				actual:$stubResult,
 			);
 
@@ -41,6 +42,33 @@
 			$this->assertEquals(
 				expected: $expectedAfterStub,
 				actual:$stubResult->afterStub,
+			);
+		}
+
+		public function testGetStubWithMultipleOccurrences(): void{
+			$query = "Rapid Response Plumbing, Heating & Air";
+			$queryMultiByteLength = mb_strlen($query);
+			$testString = <<<HTML
+			<title>3 Benefits Of High Pressure Water Jetting For Your Home’s Pipes | Glenwood Plumber | Rapid Response Plumbing, Heating & Air  ’</title>
+			<meta name="description" property="og:description" content="’If you’re one of the many Glenwood homeowners out there with a particularly stubborn drain clog on your hands, it might be time to give up on those store-bought drain cleaning solutions and DIY rooters. In this article, our’ team of skilled plumbers here at Rapid Response Plumbing, Heating & Air are going to highlight just a few of the benefits of high pressure water jetting, and how it could be the drain cleaning solution you’ve been looking for.">
+			HTML;
+
+			$multibyte = new MultiByteString($testString);
+			$matches = $multibyte->findAllOccurrences($query);
+
+			$stubResult = $multibyte->getSubStringWithPadding(
+				start: $matches[0][1][1],
+				stubMultiByteLength: $queryMultiByteLength,
+			);
+
+			$this->assertInstanceOf(
+				expected: StubResult::class,
+				actual:$stubResult,
+			);
+
+			$this->assertEquals(
+				expected: $query,
+				actual:$stubResult->stub,
 			);
 		}
 	}
